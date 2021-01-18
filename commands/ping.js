69352491b37549093b0143344afdce528649783b
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-01-18 12:51:13 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,10 @@
+const discord = require('discord.js')
+const config = require('../config.json')
+
 module.exports.run = async(client,message,args)=>{
-  message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+  const messageEmbed = new discord.MessageEmbed()
+    .setColor('#55CDFC')
+    .setAuthor(config.bot_name)
+    .setTitle(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+  message.channel.send(messageEmbed)
 }
