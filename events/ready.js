361ab803b5976b0b3744c4ae79f9718af7fe161e
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-01-18 22:45:21 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const config = require('../config.json')
 
 module.exports = (client,message) => {
   console.log("Bot Started")
-  client.user.setActivity(`I answer by the prefix "${config.prefix}",  try using "${config.prefix}help".`)
+  client.user.setActivity(`I answer by the prefix "${config.prefix}",  try using "${config.prefix}ping".`)
 }
